499f2e5a60a37781bc36d0732226c8e60b30f80f
Larry Price <larry.price.dev@gmail.com>
2017-01-26 21:21:08 -0500

    Find documents more randomly (Fixes #8) (#12)

--- mongoose-simple-random.js
+++ mongoose-simple-random.js
@@ -1,75 +1,35 @@
-module.exports = exports = function (schema) {
-  schema.statics.findRandom = function (conditions, fields, options, callback) {
-    var args = checkParams(conditions, fields, options, callback);
-
-    var limit = 1;
-    var populate;
-
-    if (args.options.limit) {
-      limit = args.options.limit;
-      delete args.options.limit;
+// Utility methods - some were pulled partially from other MIT-licensed projects
+var utils = (function () {
+  var random = function(max) { return Math.floor(Math.random() * (max+1)); };
+  var shuffle = function(a) {
+    var length = a.length,
+        shuffled = Array(length);
+    for (var index = 0, rand; index < length; ++index) {
+      rand = random(index);
+      if (rand !== index) shuffled[index] = shuffled[rand];
+      shuffled[rand] = a[index];
     }
-
-    if (args.options.populate) {
-      populate = args.options.populate;
-      delete args.options.populate;
-    }
-
-    var _this = this;
-
-    _this.count(args.conditions, function (err, num) {
-      if (err) {
-        return args.callback(err, undefined);
-      }
-      if (limit > num) {
-        limit = num;
-      }
-      var start = Math.floor(Math.random() * (num - limit + 1));
-      args.options.skip = start;
-      args.options.limit = limit;
-      var find = _this.find(args.conditions, args.fields, args.options);
-      if (populate) {
-        find.populate(populate);
-      }
-      find.exec(function (err, docs) {
-        if (err) {
-          return args.callback(err, undefined);
-        }
-        return args.callback(undefined, docs);
-      });
-    });
+    return shuffled;
   };
 
-  schema.statics.findOneRandom = function (conditions, fields, options, callback) {
-    var args = checkParams(conditions, fields, options, callback);
-    var populate;
+  var range = function(length) {return Array(length).fill(null).map(function(cv, i) {return i}); };
+  var sample = function(a, n) { return shuffle(a).slice(0, Math.max(0, n)); };
+  var randomMap = function(count, limit, next, callback) { return asyncMap(sample(range(count), limit), next, callback); };
 
-    if (args.options.limit) {
-      delete args.options.limit;
-    }
+  var asyncMap = function(items, next, callback) {
+    var transformed = new Array(items.length),
+        count = 0,
+        halt = false;
 
-    if (args.options.populate) {
-      populate = args.options.populate;
-      delete args.options.populate;
-    }
-
-    var _this = this;
-
-    _this.count(args.conditions, function (err, num) {
-      if (err) {
-        return args.callback(err, undefined);
-      }
-      var start = Math.floor(num * Math.random());
-      args.options.skip = start;
-      var find = _this.findOne(args.conditions, args.fields, args.options);
-      if (populate) {
-        find.populate(populate);
-      }
-      find.exec(function (err, doc) {
-        if (err) {
-          return args.callback(err, undefined);
+    items.forEach(function(item, index) {
+      next(item, function(error, transformedItem) {
+        if (halt) return;
+        if (error) {
+          halt = true;
+          return callback(error);
         }
-        return args.callback(undefined, doc);
+        transformed[index] = transformedItem;
+        if (++count === items.length) return callback(undefined, transformed);
       });
     });
   };
@@ -99,5 +59,70 @@ module.exports = exports = function (schema) {
       options: options,
       callback: callback
     }
+  };
+
+  return {
+    randomMap: randomMap,
+    checkParams: checkParams
+  };
+}());
+
+module.exports = exports = function (schema) {
+  schema.statics.findRandom = function (conditions, fields, options, callback) {
+    var args = utils.checkParams(conditions, fields, options, callback),
+        _this = this;
+
+    return _this.count(args.conditions, function(err, count) {
+      var limit = 1,
+        populate = null;
+      if (err) {
+        return args.callback(err, undefined);
+      }
+      if (args.options.limit) {
+        limit = args.options.limit;
+        delete args.options.limit;
+      }
+      if (limit > count) {
+        limit = count;
+      }
+      if (args.options.populate) {
+        populate = args.options.populate;
+        delete args.options.populate;
+      }
+
+      return utils.randomMap(count, limit, (item, next) => {
+        args.options.skip = item;
+        var find = _this.findOne(args.conditions, args.fields, args.options);
+        if (populate) {
+          find.populate(populate);
+        }
+        find.exec(function (err, doc) {
+          if (err) {
+            return next(err);
+          }
+          return next(undefined, doc);
+        });
+      }, (error, contents) => {
+        if (error) { return args.callback(error); }
+        return args.callback(undefined, contents);
+      });
+    });
+  };
+
+  schema.statics.findOneRandom = function (conditions, fields, options, callback) {
+    var args = utils.checkParams(conditions, fields, options, callback);
+
+    args.options.limit = 1;
+    this.findRandom(args.conditions, args.fields, args.options, function(err, docs) {
+      if (docs.length === 1) {
+        return args.callback(err, docs[0]);
+      }
+
+      if (!err) {
+        err = "findOneRandom yielded no results.";
+      }
+
+      return args.callback(err);
+    });
   };
 };
